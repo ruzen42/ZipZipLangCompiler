ee61330886095fd323b0a46bb0bbddb91c4c8bb4
ruzen42 <malledeferlive@gmail.com>
2025-08-02 00:13:30 +0500

    fix AST.hs

--- src/AST.hs
+++ src/AST.hs
@@ -1,11 +1,24 @@
-module AST (Expr) where
+module AST (Expr(..), Type, Definition(..), Program(..)) where
 
-data Expr 
-  = ENumber Integer
-  | EString String
-  | EBool Bool 
-  | EIdentifier String
+type Type = String
+
+data Expr
+  = ENumber Integer         
+  | EString String           
+  | EBool Bool                
+  | EIdentifier String         
   | EBinaryOp String Expr Expr  
-  | EIf Expr Expr Expr         
-  | ELet String Expr Expr     
+  | EIf Expr Expr Expr           
+  | ELet String Expr Expr       
+  | EFunctionCall String [Expr]  
+  | EBlock [Expr]                 
+  deriving (Show, Eq)
+
+data Definition
+  = DFunctionDef String [(String, Type)] Type Expr 
+  | DMainBlock Expr                                 
+  deriving (Show, Eq)
+
+data Program
+  = PProgram String [Definition]  
   deriving (Show, Eq)
